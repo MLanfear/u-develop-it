249e0925432527bb7da5d65669339f62e8796884
MLanfear <maxjlanfear@gmail.com>
2022-10-17 04:50:36 -0700

    established db query

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mysql = require('mysql2');
 const express = require('express');
 
 const PORT = process.env.PORT || 3001;
@@ -6,6 +7,23 @@ const app = express();
 // EXpress middleware
 app.use(express.urlencoded({ extended: false}));
 app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // Your MySql username,
+        user: 'root',
+        // Your MySql password
+        password: 'Football4200$$',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {

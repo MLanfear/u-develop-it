c5e504279ddd3f7ab73094352c38d63ac2571026
MLanfear <maxjlanfear@gmail.com>
2022-11-09 23:47:08 -0800

    updates

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+const inputCheck = require('./utils/inputCheck');
+
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -50,7 +52,7 @@ app.get('/api/candidates', (req, res) => {
 });
 
 //GET A SINGLE CANDIDATE
-app.get('/api/candidates/:id', (req, res) => {
+app.get('/api/candidate/:id', (req, res) => {
     const sql = `SELECT * FROM candidates WHERE id = ?`;
     const params = [req.params.id];
 
@@ -66,12 +68,49 @@ app.get('/api/candidates/:id', (req, res) => {
     });
 });
 // // Delete a candidate
-// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
-//     if (err) {
-//         console.log(err);
-//     }
-//     console.log(result);
-// });
+app.delete('/api.candidate/:id', (req, res) => {
+    const sql = `DELETE FROM candidates WHERE id = ?`;
+    const params = [req.params.id];
+
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.statusMessage(400).json({ error: res.message});
+        } else if (!result.affectedRows) {
+            res.json({
+                message: 'Candidate not found'
+            });
+        } else {
+            res.json({
+                message: 'deleted',
+                changes: result.affectedRows,
+                id: req.params.id
+            });
+        }
+    });
+});
+
+// CREATE A CANDIDATE
+app.post('/api/candidate', ({ body }, res) => {
+    const errors = inputCheck(body, 'first_name', 'last_name', 'industry_connected');
+    if (errors) {
+        res.status(400).json ({ error: errors });
+        return;
+    }
+    const sql = `INSERT INTO candidates (first_name, last_name, industry_connected) 
+        VALUES (?,?,?)`;
+    const params = [body.first_name, body.last_name, body.industry_connected];
+
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: body
+        });
+    });
+});
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
